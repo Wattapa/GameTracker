f6e0b4476b70a9a9d4ac6704c217bf30d717b44e
Wattapa <maxime.spitaleri@gmail.com>
2026-02-19 11:23:37 +0100

    Add folder Class and the first class Game

---
+++ Class/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTracker.Class
+{
+    class Game
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(200, ErrorMessage = "La longueur maximale ne doit pas dépasser 200 caractères")]
+        public string Title { get; set; } = "Undefined";
+        
+        public int ReleaseYear { get; set; }
+    }
+}
