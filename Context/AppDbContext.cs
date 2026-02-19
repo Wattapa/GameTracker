353d704dca2c6c2347c8c894f3835438691a1f46
TakyL <takinou57@gmail.com>
2026-02-19 12:00:43 +0100

    Update DbContext

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -10,10 +10,15 @@ namespace GameTracker.Context
 {
     internal class AppDbContext : DbContext
     {
-        public DbSet<Game> games;
+        public DbSet<Game> games { get; set; }
 
-        public DbSet<Category> categories;
+        public DbSet<Category> categories { get; set; }
 
+        public DbSet<User> users { get; set; }
+
+        public DbSet<PlaySession> playSessions { get; set; }
+
+        public DbSet<Studio> studios { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
